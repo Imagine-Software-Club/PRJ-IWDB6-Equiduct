70819dc5e4e0a2dd5b03510f957ad0c3be4f97ca
voirvoir <phucnhakh@gmail.com>
2024-02-14 20:00:44 -0500

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -89,6 +89,7 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Explore starter templates for Next.js.
           </p>
+          <p>Hoang is cool</p>
         </a>
 
         <a
